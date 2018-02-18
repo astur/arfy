9c6d05db0f8e636509b4ab69e55d43d0eaef4253
astur <astur@yandex.ru>
2018-02-18 20:54:25 +0300

    simple tests

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ const test = require('ava');
 const arfy = require('.');
 
 test('arfy', t => {
-    t.true(true);
-    t.is(arfy, arfy);
+    t.deepEqual(arfy('test'), ['test']);
+    t.deepEqual(arfy(['test']), ['test']);
+    t.deepEqual(arfy(['test1', 'test2']), ['test1', 'test2']);
+    t.deepEqual(arfy(), []);
+    t.deepEqual(arfy(null), []);
 });
