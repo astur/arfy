b1d55f4d3009fac0c5bbc846a9e26f2ebe0be883
astur <astur@yandex.ru>
2018-04-27 15:03:13 +0300

    test for multi-arg

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
 const test = require('ava');
 const arfy = require('.');
 
-test('arfy', t => {
+test('single', t => {
     t.deepEqual(arfy('test'), ['test']);
     t.deepEqual(arfy(['test']), ['test']);
     t.deepEqual(arfy(['test1', 'test2']), ['test1', 'test2']);
     t.deepEqual(arfy(), []);
     t.deepEqual(arfy(null), []);
+});
+
+test('multi', t => {
+    t.deepEqual(arfy('test1', 'test2'), ['test1', 'test2']);
+    t.deepEqual(arfy(['test1'], ['test2']), ['test1', 'test2']);
+    t.deepEqual(arfy('test1', ['test2', 'test3']), ['test1', 'test2', 'test3']);
+    t.deepEqual(arfy(['test1', 'test2'], [], null), ['test1', 'test2']);
+    t.deepEqual(arfy(['test', ['test']]), ['test', ['test']]);
 });
