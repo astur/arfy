46ef22eb4bf6a479a61bc0554c1271efed805c06
astur <astur@yandex.ru>
2018-06-05 23:19:32 +0300

    test for different types

--- test.js
+++ test.js
@@ -16,3 +16,10 @@ test('multi', t => {
     t.deepEqual(arfy(['test1', 'test2'], [], null), ['test1', 'test2']);
     t.deepEqual(arfy(['test', ['test']]), ['test', ['test']]);
 });
+
+test('types', t => {
+    t.deepEqual(arfy(123), [123]);
+    t.deepEqual(arfy({a: 1, b: 2}), [{a: 1, b: 2}]);
+    t.deepEqual(arfy(true), [true]);
+    t.deepEqual(arfy(false), [false]);
+});
